63312a8db9a5335df1fb8072e63194dcbdeaf24d
Yogesh D <yogdat@gmail.com>
2026-09-14 19:48:35 +0530

    moved frontend code to client folder and created server folder for backend code

--- client/src/core/main.tsx
+++ client/src/core/main.tsx
@@ -8,7 +8,7 @@ async function enableMocking() {
     return;
   }
 
-  const { worker } = await import('../core/mocks/browser');
+  const { worker } = await import('./mocks/browser.ts');
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
