00255f113606a8bbf863be0e16c6da7e6d6b491d
Yogesh D <yogdat@gmail.com>
2026-09-14 20:43:07 +0530

    Initialized express backend with postgres

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+// src/index.ts
+import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+import { pool } from './db';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/test-db', async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ success: true, time: result.rows[0].now });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ success: false, error: 'Database connection failed' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
